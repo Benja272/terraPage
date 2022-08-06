0043b00fc9a2eec740bf5a1bd85badd2a1e28ceb
bmartinez4 <bmartinez@seenka.com>
2022-08-06 15:59:59 -0300

    fixing cache

--- backServer/settings.py
+++ backServer/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'vistas/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -152,7 +152,6 @@ LOGGING = {
     },
 }
 
-STATIC_URL = 'vistas/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"backServer/vistas")
