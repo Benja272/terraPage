0502b33110440e8ae49c33a5a93e2900f706001a
bmartinez4 <bmartinez@seenka.com>
2022-08-14 19:28:33 -0300

    models continue

--- polls/models.py
+++ polls/models.py
@@ -1,26 +1,21 @@
-from platform import mac_ver
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
-class Operator(User):
-    name = models.CharField(primary_key=True, default="",max_length=60)
-
-    def __str__(self):
-        return self.first_name
+from django.contrib.postgres.fields import ArrayField
 
 
 class Flote(models.Model):
-    name = models.CharField(max_length=30)
-    code = models.CharField(primary_key=True, max_length=30)
-    brand = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+    code = models.CharField(primary_key=True, max_length=50)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
     patent = models.CharField(max_length=20)
     engine_n = models.IntegerField(null=True, blank=True)
     chassis_n = models.IntegerField(null=True, blank=True)
     production_year = models.DateField(null=True, blank=True)
     status = models.IntegerField(default=1)
     justifyStatus = models.CharField(max_length=300, blank=True, default="")
-    operator = models.ManyToManyField(Operator, default="", blank=True)
+    operators = ArrayField(models.CharField(max_length=50, blank=True), default=[])
+    views = ArrayField(models.ImageField(), blank=True, default=[])
 
     def __str__(self):
         return "%s %s" % (self.name, self.code)
