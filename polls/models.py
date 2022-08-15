dcb9f6feff4a3152f7cd671e392b339bd2a7640f
bmartinez4 <bmartinez@seenka.com>
2022-08-15 19:13:42 -0300

    adding set to limit attr

--- polls/models.py
+++ polls/models.py
@@ -2,30 +2,39 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
+STATES = [
+    ('GRE', 'GREEN'),
+    ('YEL', 'YELLOW'),
+    ('RED', 'RED')
+]
 
 class Flote(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(primary_key=True, max_length=50)
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
+    characteristics = models.CharField(max_length=600)
     patent = models.CharField(max_length=20)
-    engine_n = models.IntegerField(null=True, blank=True)
-    chassis_n = models.IntegerField(null=True, blank=True)
     production_year = models.DateField(null=True, blank=True)
-    status = models.IntegerField(default=1)
-    justifyStatus = models.CharField(max_length=300, blank=True, default="")
+    engine_number = models.IntegerField(null=True, blank=True)
+    chassis_number = models.IntegerField(null=True, blank=True)
+    status = models.CharField(choices=STATES)
+    justifyStatus = models.CharField(max_length=600, blank=True, default="")
     operators = ArrayField(models.CharField(max_length=50, blank=True), default=[])
     views = ArrayField(models.ImageField(), blank=True, default=[])
 
     def __str__(self):
-        return "%s %s" % (self.name, self.code)
+        return "%s %s" % (self.name, self.code, self.status)
 
-
-class Repair(models.Model):
-    type = models.IntegerField()
+MAINTENANCE_TYPES = [
+    ('REP', 'REPAIR'),
+    ('MAN', 'MAINTENANCE')
+]
+class Maintenance(models.Model):
+    type = models.CharField(choices=MAINTENANCE_TYPES)
     date = models.DateField()
     mileage = models.IntegerField() #kilometraje
-    description = models.CharField(max_length=300)
+    description = models.CharField(max_length=600)
     cost = models.FloatField()
     flote = models.ForeignKey(Flote, on_delete=models.CASCADE)
 
@@ -34,4 +43,4 @@ class Repair(models.Model):
         ordering = ['date']
 
     def __str__(self):
-        return "%s %s" % (self.date, self.flote.__str__())
+        return "%s %s" % (self.type, self.date, self.flote.__str__())
