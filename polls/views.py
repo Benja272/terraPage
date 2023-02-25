a56cbc2689ad991a6114fed465ea1b2f235b6b6d
Benjand272 <bm0130647@gmail.com>
2023-02-25 20:32:55 -0300

    rest of checge

--- polls/views.py
+++ polls/views.py
@@ -56,6 +56,7 @@ def get_flotes_page(request):
         print("/home/flotes/" + request.POST['code'])
         return redirect("/home/flotes/" + request.POST['code'])
     json_flotes = flotes()
+    print(json_flotes)
     correct_group = False
     if request.user.groups.all()[0].name == 'admin':
         correct_group = True
